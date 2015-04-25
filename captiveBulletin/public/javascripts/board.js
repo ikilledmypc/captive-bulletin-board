27a48ebbd906559e3cfcb9a63d0a1df06dac6acc
tim <timsmeets@rocketmail.com>
2015-04-25 23:13:05 +0200

    messages working with masonry

--- captiveBulletin/public/javascripts/board.js
+++ captiveBulletin/public/javascripts/board.js
@@ -9,12 +9,26 @@ function start(){
    var $container = $("#board").masonry();
 
   for(var i=0;i<10 ; i++){
-    var hRand = Math.random();
-    var elem = document.createElement('div');
-    var heightClass = hRand > 0.85 ? 'h4' : hRand > 0.6 ? 'h3' : hRand > 0.35 ? 'h2' : '';
-    elem.className = 'item ' + heightClass;
 
-    $container.append( elem ).masonry( 'appended', elem );
+    var hRand = Math.random() *100;
+
+    var elem = document.createElement('div');
+    elem.className = 'note panel panel-default';
+
+    var header = document.createElement('div');
+    header.className = 'panel-heading';
+    $(header).append('this is a title');
+    $(elem).append(header);
+
+    var body = document.createElement('div');
+    body.className = 'panel-body';
+    $(elem).append(body);
+
+    for(var word =0; word <hRand; word++){
+      $(body).append(' bla ');
+    }
+    $container.append( elem ).masonry( 'appended', elem ).masonry();
+    //$container.masonry( 'addItems', elem );
   }
 
 }
