3a9d4e2d64da566f4bb859dc4331c22229204457
Tim Smeets <timsmeets@rocketmail.com>
2015-05-19 13:49:47 +0200

    Update posts.js

--- captiveBulletin/routes/posts.js
+++ captiveBulletin/routes/posts.js
@@ -6,7 +6,6 @@ mongoose.connect('mongodb://localhost/bulletinboard');
 
 var Post = mongoose.model('posts', { title: String, content : String });
 
-/* POST users listing. */
 router.post('/', function(req, res, next) {
   var post = new Post(req.body);
   post.save(function (err) {
